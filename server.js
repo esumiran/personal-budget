67fd79611af120dae39b6af7802f521ac5f2ddea
E_Sumiran <e.sumiran@outlook.com>
2024-02-04 18:04:49 -0500

    Loading data from JSON file

--- server.js
+++ server.js
@@ -1,34 +1,27 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 const port = 3000;
 
 app.use('/', express.static('public'));
 
-const budget = {
-    myBudget: [
-    {
-        title: 'Eat out',
-        budget: 40
-    },
-   
-    {
-        title: 'Rent',
-        budget: 400 
-    },
-    {
-        title: 'Groceries',
-        budget: 100
-    },
- ]
-};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
+
 app.get('/budget', (req, res) => {
-    res.json(budget);
+    fs.readFile('budget.json', 'utf8', (err, data) => {
+        if (err) {
+            console.error(err);
+            return res.status(500).send('An error occurred while reading the budget file.');
+        }
+        res.setHeader('Content-Type', 'application/json');
+        res.send(data);
+    });
 });
+
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
