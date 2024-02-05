731e00565dd27edee622a84c285402cbb9d1ab79
E_Sumiran <e.sumiran@outlook.com>
2024-02-04 22:36:40 -0500

    Added D3JS chart

--- server.js
+++ server.js
@@ -1,18 +1,38 @@
 const express = require('express');
-const fs = require('fs');
 const app = express();
+const fs = require('fs');
 const port = 3000;
 
 app.use('/', express.static('public'));
 
+const budget = {
+    myBudget: [
+    {
+        title: 'Eat out',
+        budget: 40
+    },
+
+    {
+        title: 'Rent',
+        budget: 400 
+    },
+    {
+        title: 'Groceries',
+        budget: 100
+    },
+ ]
+};
 
 app.get('/hello', (req, res) => {
     res.send('Hello World!');
 });
 
-
 app.get('/budget', (req, res) => {
-    fs.readFile('budget.json', 'utf8', (err, data) => {
+    res.json(budget);
+});
+
+app.get('/doughnut', (req, res) => {
+    fs.readFile('doughnut.json', 'utf8', (err, data) => {
         if (err) {
             console.error(err);
             return res.status(500).send('An error occurred while reading the budget file.');
@@ -21,7 +41,6 @@ app.get('/budget', (req, res) => {
         res.send(data);
     });
 });
-
 
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`);
